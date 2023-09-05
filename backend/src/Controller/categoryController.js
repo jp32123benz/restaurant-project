1cf6d03e4d279e2db2f6244e3d01b7f89f3330ff
chintan-benzatine <interview.benzatine@gmail.com>
2023-09-05 18:33:21 +0530

    Create categoryController.js

---
+++ backend/src/Controller/categoryController.js
@@ -0,0 +1,30 @@
+const Category = require('../models/categoryModel')
+const User = require('../models/userModel')
+
+module.exports = {
+    createCategory: async (req, res) => {
+        try {
+            const isExist = await User.findOne({ name: req.body.name })
+            if (isExist) {
+                return res.status(400).json({ statusCode: 400, msg: "Category already Exists" })
+            } else {
+                const data = new Category(req.body)
+                await data.save()
+                return res.status(200).json({ statusCode: 200, msg: "Name Registered Succesfully" })
+            }
+        } catch (err) {
+            return res.status(400).json({ statusCode: 400, err, msg: "Please enter correct details" })
+        }
+    },
+    getCategory: async (req, res) => {
+        try {
+            const findData = await Category.find();
+            if (findData != [] && findData)
+                res.status(201).json({ statusCode: 201, findData });
+            else
+                throw new Error
+        } catch (err) {
+            res.status(400).json({ statusCode: 400, err, msg: "Data doesn't exist" });
+        }
+    },
+}
