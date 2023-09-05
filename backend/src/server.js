f885d2266627bc304d12efff26380ef4dffdf80e
chintan-benzatine <interview.benzatine@gmail.com>
2023-09-05 18:33:08 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ const userRouter = require("./router/userRouter");
 const cookieParser = require("cookie-parser");
 const restaurantRouter = require("./router/restaurantRouter");
 const restaurantFoodRouter = require("./router/restaurantFoodRouter");
+const categoryRouter = require("./router/categoryRouter");
 const cloudinary = require('cloudinary').v2
 const cors = require('cors')
 const morgan = require('morgan')
@@ -24,6 +25,7 @@ app.use(express.json({ limit: '100mb' }));
 app.use(morgan('dev'))
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/restaurant", restaurantRouter);
+app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/restaurants-food", restaurantFoodRouter);
 
 // All Routes
