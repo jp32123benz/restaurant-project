e9dd874d5ad7a8492e63eefd7e40ccae1b516de6
chintan-benzatine <interview.benzatine@gmail.com>
2023-09-06 18:04:28 +0530

    Update restaurantFoodRouter.js

--- backend/src/router/restaurantFoodRouter.js
+++ backend/src/router/restaurantFoodRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const AuthMiddleware = require("../middleware/AuthMiddleware");
-const RestaurantFood = require('../models/restaurantFoodModel')
-const multer = require("multer");
-const fs = require('fs')
+// const RestaurantFood = require('../models/restaurantFoodModel')
+const upload = require('../Controller/utilityController/multerFood')
+// const fs = require('fs')
 const {
   createRestaurantFood,
   findRestaurantFood,
@@ -13,38 +13,15 @@ const {
   deleteRestaurantFoodImage
 } = require("../Controller/restaurantFoodController");
 const { restaurantFoodValidation } = require("../middleware/restaurantValidationMiddleware");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    return cb(null, "src/restaurantFoodImages");
-  },
-  filename: function (req, file, cb) {
-    return cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-const upload = multer({
-  storage,
-  // fileFilter: (req, file, cb) => {
-  //   if (file.mimetype == 'image/jpg' ||
-  //     file.mimetype == 'image/jpeg' ||
-  //     file.mimetype == 'image/png'
-  //   ) {
-  //     cb(null, true)
-  //   } else {
-  //     cb(null, false)
-  //     return cb(Error('Only .png, .jpg and .jpeg format allowed!'))
-  //   }
-  // }
-})
+
 const router = express.Router();
 
 router.post(
-  "/create-restaurant-food",
-  restaurantFoodValidation,
-  AuthMiddleware,
+  "/create-restaurant-food",restaurantFoodValidation,AuthMiddleware,upload.array('foodImages', 4),
   createRestaurantFood
 );
 
-router.put("/update-restaurant-food-image", AuthMiddleware, upload.array('foodImages'), updateRestaurantFoodImage);
+router.put("/update-restaurant-food-image", AuthMiddleware, updateRestaurantFoodImage);
 
 router.put("/update-restaurant-food", AuthMiddleware, updateRestaurantFood);
 
@@ -55,7 +32,5 @@ router.get("/find-restaurant-food/:id", AuthMiddleware, findSingleRestaurantFood
 router.delete("/delete-restaurant-food", AuthMiddleware, deleteRestaurantFood);
 
 router.delete("/delete-restaurant-food-image", AuthMiddleware, deleteRestaurantFoodImage);
-
-
 
 module.exports = router;
