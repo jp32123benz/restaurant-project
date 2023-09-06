9ac3c4235bd3f19506f4389cc10a12e78cfb3a80
chintan-benzatine <interview.benzatine@gmail.com>
2023-09-06 18:05:01 +0530

    Update Store.js

--- frontend/src/store/Store.js
+++ frontend/src/store/Store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
-import FoodSlice from './actions/FoodSlice'
+import FoodSlice, { fetchFood } from './actions/FoodSlice'
 import userSlice from './actions/userSlice'
+import CategorySlice from './actions/CategorySlice'
 
 export const store = configureStore({
     reducer: {
-        food: FoodSlice,
-        user: userSlice
+        food: FoodSlice, // Add your reducer to the store
+        user: userSlice,
+        category: CategorySlice,
     },
+    // middleware: (getDefaultMiddleware) => {
+    //     return getDefaultMiddleware().concat(fetchFood);
+    // },
 })
