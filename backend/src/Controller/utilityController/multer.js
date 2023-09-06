3a28fdc3aa85a7d7e31fe8639a4f3694d145647a
chintan-benzatine <interview.benzatine@gmail.com>
2023-09-06 18:04:05 +0530

    Update multer.js

--- backend/src/Controller/utilityController/multer.js
+++ backend/src/Controller/utilityController/multer.js
@@ -1,13 +1,6 @@
 const multer = require('multer')
-const cloudinary = require('cloudinary').v2
+const cloudinary = require('./cloudinary')
 const { CloudinaryStorage } = require('multer-storage-cloudinary')
-
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_KEY,
-    api_secret: process.env.CLOUD_KEY_SECRET,
-    secure: true
-})
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
